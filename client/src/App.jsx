14db6a5713484799d999af93fa10222b97f90fe0
ArpitSharma19 <arpit.atrii@gmail.com>
2025-12-19 15:19:50 +0530

    Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,9 +33,9 @@ const App = () => {
         <Route path="/my-enrollments" element={<MyEnrollments />} />
         <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
-        <Route path='/educator' element={<AddCourse />}>
-          <Route path='educator' element={<Dashboard />} />
-          {/* <Route path='add-course' element={<AddCourse />} /> */}
+        <Route path='/educator' element={<Educator />}>
+          <Route path='/educator' element={<Dashboard />} />
+          <Route path='add-course' element={<AddCourse />} />
           <Route path='my-courses' element={<MyCourses />} />
           <Route path='student-enrolled' element={<StudentsEnrolled />} />
         </Route>
